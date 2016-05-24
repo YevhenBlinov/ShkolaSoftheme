3248d2a611d023e2056027a02a5bd3d863485a8c
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-05-24 20:39:41 +0300

    Added PrintNameWithYellowColor project

---
+++ SCharpHW/1/PrintNameWithYelllowColor/PrintNameWithYelllowColor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintNameWithYelllowColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Yevhen Blinov";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(fullName);
+        }
+    }
+}
