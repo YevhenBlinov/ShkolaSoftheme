73c5d45e3d6c3f04cf30bfef64c76967368f403b
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-05-28 17:59:16 +0300

    Added SimpleCalculatorProject.

---
+++ SCharpHW/2/SimpleCalculatorProject/SimpleCalculatorProject/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace SimpleCalculatorProject
+{
+    class Program
+    {
+        private readonly static char[] SimpleMathematicalOperators = {'+', '-', '*', '/'};
+        private static readonly char[] Separators = {'+', '-', '*', '/', ' '};
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter operation to do in this way: firstNumber mathematicalOperator secondNumber");
+            var operation = Console.ReadLine();
+
+            double result;
+            try
+            {
+                result = GetResultFromEnteredOperation(operation);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            Console.WriteLine(operation + " = {0}", result);
+            
+        }
+
+        private static double GetResultFromEnteredOperation(string operation)
+        {
+            var numbers = GetNumbersArrayFromEnteredOperation(operation);
+            double firstNumber, secondNumber;
+            if (numbers.Length != 2 || !double.TryParse(numbers[0], out firstNumber) || !double.TryParse(numbers[1], out secondNumber))
+            {
+                throw new Exception("Numbers must be type of double.");
+            }
+            var mathematicalOperator = GetMathematicalOperatorFromEnteredOperation(operation);
+
+            switch (mathematicalOperator)
+            {
+                case "-":
+                    return SimpleCalculator.Subtraction(firstNumber, secondNumber);
+                case "*":
+                    return SimpleCalculator.Multiplication(firstNumber, secondNumber);
+                case "/":
+                    return SimpleCalculator.Division(firstNumber, secondNumber);
+                default:
+                    return SimpleCalculator.Addition(firstNumber, secondNumber);
+            }
+        }
+
+        private static string[] GetNumbersArrayFromEnteredOperation(string operation)
+        {
+            return operation.Split(Separators).Where(num => !string.IsNullOrWhiteSpace(num)).ToArray();
+        }
+
+        private static string GetMathematicalOperatorFromEnteredOperation(string operation)
+        {
+            return
+                SimpleMathematicalOperators.Where(
+                    simpleMathematicalOperator => operation.Contains(simpleMathematicalOperator)).First().ToString();
+        }
+    }
+}
---
+++ SCharpHW/2/SimpleCalculatorProject/SimpleCalculatorProject/SimpleCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimpleCalculatorProject
+{
+    public static class SimpleCalculator
+    {
+        public static double Addition(double firstNumber, double secondNumber)
+        {
+            return Math.Round(firstNumber + secondNumber, 2);
+        }
+        public static double Subtraction(double firstNumber, double secondNumber)
+        {
+            return Math.Round(firstNumber - secondNumber, 2);
+        }
+        public static double Multiplication(double firstNumber, double secondNumber)
+        {
+            return Math.Round(firstNumber * secondNumber, 2);
+        }
+        public static double Division(double firstNumber, double secondNumber)
+        {
+            return Math.Round(firstNumber / secondNumber, 2);
+        }
+    }
+}
