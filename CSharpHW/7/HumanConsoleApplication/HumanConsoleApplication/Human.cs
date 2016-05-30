c7a7e7ece37b64feb462307ce9d0ab565137a5bf
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-05-30 20:44:03 +0300

    Added HumanConsoleApplication.

---
+++ CSharpHW/7/HumanConsoleApplication/HumanConsoleApplication/Human.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HumanConsoleApplication
+{
+    public class Human
+    {
+        private readonly int _age;
+
+        public DateTime BirthDate { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age 
+        {
+            get { return _age; }
+        }
+
+        public Human(DateTime birthDate, int age)
+                    : this(birthDate, "John", "Doe", age)
+        {
+        }
+
+        public Human(DateTime birthDate, string firstName, string lastName, int age)
+        {
+            BirthDate = birthDate;
+            FirstName = firstName;
+            LastName = lastName;
+            _age = age;
+        }
+
+        public Human()
+        {
+            BirthDate = default(DateTime);
+            FirstName = default(string);
+            LastName = default(string);
+            _age = default(int);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var anotherHuman = obj as Human;
+            return FirstName.Equals(anotherHuman.FirstName) &&
+                   LastName.Equals(anotherHuman.LastName) &&
+                   BirthDate.Equals(anotherHuman.BirthDate) &&
+                   Age == anotherHuman.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            return FirstName.GetHashCode() + LastName.GetHashCode() + BirthDate.GetHashCode() + Age.GetHashCode();
+        }
+    }
+}
---
+++ CSharpHW/7/HumanConsoleApplication/HumanConsoleApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HumanConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create first human...");
+            var firstHuman = new Human(new DateTime(1993, 04, 28), "Yevhen", "Blinov", 23);
+            Console.WriteLine("Created human with properties: First name " + firstHuman.FirstName + ", Last name " +
+                              firstHuman.LastName + ", Birth date " + firstHuman.BirthDate + ", Age " + firstHuman.Age);
+            Console.WriteLine("Create second human with unknown first and last names...");
+            var secondHuman = new Human(new DateTime(1993, 04, 28), 23);
+            Console.WriteLine("Created human with properties: First name " + secondHuman.FirstName + ", Last name " +
+                              secondHuman.LastName + ", Birth date " + secondHuman.BirthDate + ", Age " + secondHuman.Age);
+            Console.WriteLine("Create third human with default properties...");
+            var thirdHuman = new Human();
+            Console.WriteLine("Created human with properties: First name " + thirdHuman.FirstName + ", Last name " +
+                              thirdHuman.LastName + ", Birth date " + thirdHuman.BirthDate + ", Age " + thirdHuman.Age);
+            Console.WriteLine("Create forth human with same properties of first human...");
+            var forthHuman = new Human(new DateTime(1993, 04, 28), "Yevhen", "Blinov", 23);
+            Console.WriteLine("Created human with properties: First name " + forthHuman.FirstName + ", Last name " +
+                              forthHuman.LastName + ", Birth date " + forthHuman.BirthDate + ", Age " + forthHuman.Age);
+            Console.WriteLine();
+
+            Console.WriteLine("Check if the first human equals to the second human");
+            Console.WriteLine(firstHuman.Equals(secondHuman)
+                              ? "The first human equals to the second human"
+                              : "The first human doesn't equal to the second human");
+            Console.WriteLine();
+
+            Console.WriteLine("Check if the first human equals to the forth human");
+            Console.WriteLine(firstHuman.Equals(forthHuman)
+                              ? "The first human equals to the forth human"
+                              : "The first human doesn't equal to the forth human");
+        }
+    }
+}
