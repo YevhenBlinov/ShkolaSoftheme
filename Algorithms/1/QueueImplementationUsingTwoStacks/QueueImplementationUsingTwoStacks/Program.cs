6ecba20e7b04a45588421adb9da4b69fbe3d9e38
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-06-05 22:15:16 +0300

    Added QueueImplementationUsingTwoStacks.

---
+++ Algorithms/1/QueueImplementationUsingTwoStacks/QueueImplementationUsingTwoStacks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueImplementationUsingTwoStacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new QueueUsingTwoStacks<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            Console.WriteLine("Queue: ");
+            queue.Display();
+            Console.WriteLine("A peek element of the queue is {0}", queue.Peek());
+            var item = queue.Dequeue();
+            Console.WriteLine("The item {0} was removed from the queue.", item);
+            Console.WriteLine("Queue: ");
+            queue.Display();
+            item = queue.Dequeue();
+            Console.WriteLine("The item {0} was removed from the queue.", item);
+            Console.WriteLine("Queue: ");
+            queue.Display();
+            item = queue.Dequeue();
+            Console.WriteLine("The item {0} was removed from the queue.", item);
+            Console.WriteLine("Queue: ");
+            queue.Display();
+            item = queue.Dequeue();
+            Console.WriteLine("The item {0} was removed from the queue.", item);
+            queue.Display();
+        }
+    }
+}
---
+++ Algorithms/1/QueueImplementationUsingTwoStacks/QueueImplementationUsingTwoStacks/QueueUsingTwoStacks.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueImplementationUsingTwoStacks
+{
+    public class QueueUsingTwoStacks<T>
+    {
+        private Stack<T> _tailStack;
+        private Stack<T> _headerStack;
+
+        private bool IsEmpty 
+        {
+            get { return _tailStack.Count == 0 && _headerStack.Count == 0; }
+        }
+
+        public QueueUsingTwoStacks()
+        {
+            _tailStack = new Stack<T>();
+            _headerStack= new Stack<T>();
+        }
+
+        public void Enqueue(T itemToEnqueue)
+        {
+            _tailStack.Push(itemToEnqueue);
+        }
+
+        public T Dequeue()
+        {
+            FillHeaderStackIfNeeded();
+            return _headerStack.Pop();
+        }
+
+        public T Peek()
+        {
+            FillHeaderStackIfNeeded();
+            return _headerStack.Peek();
+        }
+
+        private void FillHeaderStackIfNeeded()
+        {
+            if (_headerStack.Count == 0)
+            {
+                while (_tailStack.Count != 0)
+                {
+                    _headerStack.Push(_tailStack.Pop());
+                }
+            }
+        }
+
+        public void Display()
+        {
+            if (IsEmpty)
+            {
+                Console.WriteLine("Queue is empty.");
+                return;
+            }
+
+            foreach (var item in _tailStack)
+            {
+                Console.Write(item + " ");
+            }
+
+            foreach (var item in _headerStack)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
