a38da6a52f3abda5de706d0b3fec9e0e64d44eba
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-06-05 13:49:30 +0300

    Added FindNumbersWhichOccureOddTimesInAnArray.

---
+++ CSharpHW/10/FindNumbersWhichOccureOddTimesInAnArray/FindNumbersWhichOccureOddTimesInAnArray/NumberThatOccuresOddTimesInAnArrayFinder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FindNumbersWhichOccureOddTimesInAnArray
+{
+    public static class NumberThatOccuresOddTimesInAnArrayFinder
+    {
+        public static void FindANumberThatOccuresOddTimes(this int[] array)
+        {
+            array.Sort();
+
+            var count = 1;
+            var number = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] == number)
+                {
+                    count++;
+                    if (i != array.Length - 1)
+                        continue;
+
+                    if (!IsCountAnEvenNumber(count))
+                    {
+                        DisplayANumberThatOccuresOddTimes(number, count);
+                    }
+                    else
+                    {
+                        Console.WriteLine("All numbers occure even times in array.");
+                    }
+                }
+                else
+                {
+                    if (!IsCountAnEvenNumber(count))
+                    {
+                        DisplayANumberThatOccuresOddTimes(number, count);
+                        break;
+                    }
+                    number = array[i];
+                    count = 1;
+                }
+            }
+        }
+
+        private static bool IsCountAnEvenNumber(int count)
+        {
+            return count % 2 == 0;
+        }
+
+        private static void DisplayANumberThatOccuresOddTimes(int number, int count)
+        {
+            Console.WriteLine("Number {0} occures {1} times.", number, count);
+        }
+    }
+}
---
+++ CSharpHW/10/FindNumbersWhichOccureOddTimesInAnArray/FindNumbersWhichOccureOddTimesInAnArray/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FindNumbersWhichOccureOddTimesInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] {1, 3, 2, 6, 7, 1, 8, 8, 5, 4, 3, 4, 9, 7, 2, 6, 9, 5, 0, 9, 0};
+            array.FindANumberThatOccuresOddTimes();
+        }     
+    }
+}
