67e5e4c51748581fd5d6e5f991b13ed4dd085b3f
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-06-05 14:52:28 +0300

    Added ArrayExtensionConsoleApplication.

---
+++ CSharpHW/10/ArrayExtensionConsoleApplication/ArrayExtensionConsoleApplication/ExtendedArray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayExtensionConsoleApplication
+{
+    public class ExtendedArray
+    {
+        private int[] _internalArray;
+
+        public ExtendedArray(int[] array)
+        {
+            _internalArray = array;
+        }
+
+        public void AddRange(params int[] rangeOfInts)
+        {
+            var lengthOfAnOldArray = _internalArray.Length;
+            var lenthOfParams = rangeOfInts.Length;
+            var lengthOfANewArray = lengthOfAnOldArray + lenthOfParams;
+            var newArray = new int[lengthOfANewArray];
+
+            for (int i = 0; i < lengthOfAnOldArray; i++)
+            {
+                newArray[i] = _internalArray[i];
+            }
+
+            for (int i = lengthOfAnOldArray; i < lengthOfANewArray; i++)
+            {
+                newArray[i] = rangeOfInts[i - lengthOfAnOldArray];
+            }
+
+            _internalArray = newArray;
+        }
+
+        public int GetByIndex(int index)
+        {
+            return _internalArray[index];
+        }
+
+        public bool Contains(int number)
+        {
+            return _internalArray.Any(element => element == number);
+        }
+
+        public void Display()
+        {
+            var count = 0;
+
+            foreach (var element in _internalArray)
+            {
+                if (count == 4)
+                {
+                    Console.Write(element + Environment.NewLine);
+                    count = 0;
+                }
+                else
+                {
+                    Console.Write(element + " ");
+                    count++;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CSharpHW/10/ArrayExtensionConsoleApplication/ArrayExtensionConsoleApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArrayExtensionConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myArray = new ExtendedArray(new[] {1, 2, 3, 4, 5});
+            Console.WriteLine("Array of ints was created: ");
+            myArray.Display();
+            Console.WriteLine("Let's add 6, 7, 8, 9 to the array.");
+            myArray.AddRange(6, 7, 8, 9);
+            myArray.Display();
+            Console.WriteLine();
+            Console.WriteLine("Let's get a number from the array by index 3");
+            var number = myArray.GetByIndex(3);
+            Console.WriteLine("It's {0}", number);
+            Console.WriteLine();
+            Console.WriteLine("Let's check if the array contains a number 5");
+            Console.WriteLine("Answer: {0}", myArray.Contains(5));
+            Console.WriteLine();
+            Console.WriteLine("Let's check if the array contains a number 10");
+            Console.WriteLine("Answer: {0}", myArray.Contains(10));
+            Console.WriteLine();
+        }    
+    }
+}
