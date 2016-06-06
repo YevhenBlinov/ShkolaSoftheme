49e3b803e04b352bc5134f74282f9337ebac8ba5
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-06-06 21:08:49 +0300

    Added SelectionSortConsoleApplication.

---
+++ Algorithms/2/SelectionSortConsoleApplication/SelectionSortConsoleApplication/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿namespace SelectionSortConsoleApplication
+{
+    public static class SelectionSort
+    {
+        public static void Sort(this int[] arrayToSort)
+        {
+            for (int i = 0; i < arrayToSort.Length; i++)
+            {
+                var minElementIndex = i;
+
+                for (int j = i; j < arrayToSort.Length; j++)
+                {
+                    if (arrayToSort[j] < arrayToSort[minElementIndex])
+                    {
+                        minElementIndex = j;
+                    }
+                }
+
+                Swap(arrayToSort, i, minElementIndex);
+            }
+        }
+
+        private static void Swap(int[] arrayToSort, int firstIndex, int secondIndex)
+        {
+            var temp = arrayToSort[firstIndex];
+            arrayToSort[firstIndex] = arrayToSort[secondIndex];
+            arrayToSort[secondIndex] = temp;
+        }
+    }
+}
