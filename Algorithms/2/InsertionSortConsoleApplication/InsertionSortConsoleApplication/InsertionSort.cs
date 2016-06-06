e1bf005dcfd66db9cda7432d81d258b54d335848
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-06-06 21:29:50 +0300

    Added InsertionSortConsoleApplication.

---
+++ Algorithms/2/InsertionSortConsoleApplication/InsertionSortConsoleApplication/InsertionSort.cs
@@ -0,0 +1,22 @@
+﻿namespace InsertionSortConsoleApplication
+{
+    public static class InsertionSort
+    {
+        public static void Sort(this int[] arrayToSort)
+        {
+            for (int i = 1; i < arrayToSort.Length; i++)
+            {
+                var currentNumber = arrayToSort[i];
+                var j = i;
+
+                while (j > 0 && arrayToSort[j - 1] > currentNumber)
+                {
+                    arrayToSort[j] = arrayToSort[j - 1];
+                    j--;
+                }
+
+                arrayToSort[j] = currentNumber;
+            }
+        }
+    }
+}
---
+++ Algorithms/2/InsertionSortConsoleApplication/InsertionSortConsoleApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InsertionSortConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayToSort = new[] { 9, 2, 6, 7, 1, 4, 8, 5, 0, 3 };
+            Console.WriteLine("The unsorted array:");
+            DisplayAnArray(arrayToSort);
+            Console.WriteLine();
+
+            arrayToSort.Sort();
+            Console.WriteLine("The sorted array:");
+            DisplayAnArray(arrayToSort);
+            Console.WriteLine();
+        }
+
+        private static void DisplayAnArray(int[] array)
+        {
+            var count = 0;
+
+            foreach (var element in array)
+            {
+                if (count == 4)
+                {
+                    Console.Write(element + Environment.NewLine);
+                    count = 0;
+                }
+                else
+                {
+                    Console.Write(element + " ");
+                    count++;
+                }
+            }
+        }
+    }
+}
