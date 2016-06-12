46db00763faae6cf5a3dd69f596fa1a910012f16
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-06-12 22:01:47 +0300

    Added LibraryConsoleApplication.

---
+++ CSharpHW/19/LibraryConsoleApplication/LibraryConsoleApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LibraryConsoleApplication
+{
+    class Program
+    {
+        private static List<Book> _booksList;
+        private static HashSet<User> _usersList;
+        private static bool _wantsToAuthorize;
+        private static User _lastUser;
+
+        static void Main(string[] args)
+        {
+            var library = new Library();
+            var wantsToAuthorize = library.DoesUserWantToAuthorize();
+
+            if (wantsToAuthorize)
+            {
+                library.AuthorizeAUser();
+            }
+
+            library.DisplayTheLibraryMenu();
+
+        }
+
+        
+    }
+}
