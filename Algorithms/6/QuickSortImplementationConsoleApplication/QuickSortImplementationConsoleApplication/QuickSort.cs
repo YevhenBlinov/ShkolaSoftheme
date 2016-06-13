d211f9f0306f0e5ceb4a23480b2c64f0ebd384ce
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-06-13 08:09:04 +0300

    Added QuickSortImplementationConsoleApplication.

---
+++ Algorithms/6/QuickSortImplementationConsoleApplication/QuickSortImplementationConsoleApplication/QuickSort.cs
@@ -0,0 +1,32 @@
+﻿namespace QuickSortImplementationConsoleApplication
+{
+    public static class QuickSort
+    {
+        public static void Sort(this int[] arrayToSort, int left, int right)
+        {
+            if(left >= right)
+                return;
+
+            var middle = left;
+
+            for (int i = left + 1; i <= right; i++)
+            {
+                if (arrayToSort[i] < arrayToSort[left])
+                {
+                    Swap(arrayToSort, ++middle, i);
+                }
+            }
+
+            Swap(arrayToSort, left, middle);
+            arrayToSort.Sort(left, middle - 1);
+            arrayToSort.Sort(middle + 1, right);
+        }
+
+        private static void Swap(int[] array, int firstIndex, int secondIndex)
+        {
+            var temp = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = temp;
+        }
+    }
+}
