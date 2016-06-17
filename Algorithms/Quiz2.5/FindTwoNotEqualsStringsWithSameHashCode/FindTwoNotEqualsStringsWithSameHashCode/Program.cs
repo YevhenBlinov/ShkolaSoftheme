4c6d2b0410f679df9715f60fa2f170cf21d68c83
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-06-17 14:10:07 +0300

    Added FindTwoNotEqualsStringsWithSameHashCode to the Algorithms.

---
+++ Algorithms/Quiz2.5/FindTwoNotEqualsStringsWithSameHashCode/FindTwoNotEqualsStringsWithSameHashCode/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindTwoNotEqualsStringsWithSameHashCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Find();
+        }
+
+        private static void Find()
+        {
+            var number = 0;
+            var stringsDictionary = new Dictionary<int, string>();
+
+            while (true)
+            {
+                number++;
+                var numberInStringRepresentation = number.ToString();
+                var numberInStringRepresentationHashCode = numberInStringRepresentation.GetHashCode();
+
+                try
+                {
+                    stringsDictionary.Add(numberInStringRepresentationHashCode, numberInStringRepresentation);
+                }
+                catch (Exception)
+                {
+                   
+                    Console.WriteLine("The string {0} has HashCode = {1} and the string {2} has the same HashCode = {3}", 
+                                      stringsDictionary[numberInStringRepresentationHashCode], 
+                                      stringsDictionary[numberInStringRepresentationHashCode].GetHashCode(), 
+                                      numberInStringRepresentation, 
+                                      numberInStringRepresentationHashCode);
+                    break;
+                }
+            }
+        }
+    }
+}
