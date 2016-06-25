bd9ab55af8f95b025f7ef086bd709e1d7a076686
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-06-25 22:53:25 +0300

    Added MultithreadArchiverConsoleApplication to CSharpHW.

---
+++ CSharpHW/25/MultithreadArchiverConsoleApplication/MultithreadArchiverConsoleApplication/Archiver.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Threading;
+
+namespace MultithreadArchiverConsoleApplication
+{
+    public class Archiver
+    {
+        public void ArchivateTheFolder(string pathToFolder)
+        {
+            var subDirectories = Directory.GetDirectories(pathToFolder);
+            var files = Directory.GetFiles(pathToFolder);
+
+            foreach (var file in files)
+            {
+                var newThread = new Thread(ArchivateTheFile);
+                newThread.Start(file);
+            }
+
+            if (subDirectories.Length == 0) 
+                return;
+
+            foreach (var subDirectory in subDirectories)
+            {
+                ArchivateTheFolder(subDirectory);
+            }
+        }
+
+        private void ArchivateTheFile(object pathToFileObject)
+        {
+            var pathToFile = pathToFileObject as string;
+            if(pathToFile == null)
+                return;
+
+            var pathToFolder = Path.GetDirectoryName(pathToFile);
+            var currentDirectoryName = Directory.GetCurrentDirectory();
+            var currentDirectory = new DirectoryInfo(currentDirectoryName);
+            var directoryWithFile = new DirectoryInfo(pathToFolder);            
+            var fileToArchiveNameWithoutExtension = Path.GetFileNameWithoutExtension(pathToFile);
+            var fileToArchiveFullName = Path.GetFileName(pathToFile);
+            var directoryWithArchives = Directory.CreateDirectory(currentDirectory + "\\" + "DirectoryWithArchives");
+            var createdSubDirectory = directoryWithFile.CreateSubdirectory(fileToArchiveNameWithoutExtension);
+            File.Copy(pathToFile, createdSubDirectory.FullName + "\\" + fileToArchiveFullName);
+            ZipFile.CreateFromDirectory(createdSubDirectory.FullName, directoryWithArchives.FullName + "\\" + fileToArchiveNameWithoutExtension + ".zip");
+            DeleteCreatedSubDirectoryWithIncludedFile(createdSubDirectory.FullName, fileToArchiveFullName);
+        }
+
+        private void DeleteCreatedSubDirectoryWithIncludedFile(string pathToDirectory, string includedFileName)
+        {
+            File.Delete(pathToDirectory + "\\" + includedFileName);
+            Directory.Delete(pathToDirectory);
+        }
+    }
+}
---
+++ CSharpHW/25/MultithreadArchiverConsoleApplication/MultithreadArchiverConsoleApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace MultithreadArchiverConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var archiver = new Archiver();
+            var isDerictoryExists = false;
+            var pathToFolder = string.Empty;
+
+            while (!isDerictoryExists)
+            {
+                Console.WriteLine("Please, enter a path to the folder, which will be archived.");
+                var enteredPathToFolder = Console.ReadLine();
+
+                if (!CheckIfFolderExists(enteredPathToFolder))
+                {
+                    Console.WriteLine("Sorry, but the folder doesn't exist. Please, try again.");
+                }
+                else
+                {
+                    isDerictoryExists = true;
+                    pathToFolder = enteredPathToFolder;
+                }
+            }
+
+            archiver.ArchivateTheFolder(pathToFolder);
+        }
+
+        private static bool CheckIfFolderExists(string pathToFolder)
+        {
+            return Directory.Exists(pathToFolder);
+        }
+    }
+}
