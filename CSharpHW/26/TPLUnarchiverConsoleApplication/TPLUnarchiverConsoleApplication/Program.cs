c1c8176800ac4146aa72031a6162f74dd67e77a8
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-06-25 23:28:09 +0300

    Added TPLUnarchiverConsoleApplication to CSharpHW.

---
+++ CSharpHW/26/TPLUnarchiverConsoleApplication/TPLUnarchiverConsoleApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace TPLUnarchiverConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var unarchiver = new Unarchiver();
+            var isDerictoryExists = false;
+            var pathToFolder = string.Empty;
+
+            while (!isDerictoryExists)
+            {
+                Console.WriteLine("Please, enter a path to the folder, which will be archived.");
+                var enteredPathToFolder = Console.ReadLine();
+
+                if (!CheckIfFolderExists(enteredPathToFolder))
+                {
+                    Console.WriteLine("Sorry, but the folder doesn't exist. Please, try again.");
+                }
+                else
+                {
+                    isDerictoryExists = true;
+                    pathToFolder = enteredPathToFolder;
+                }
+            }
+
+            unarchiver.UnarchiveTheFolder(pathToFolder);
+        }
+
+        private static bool CheckIfFolderExists(string pathToFolder)
+        {
+            return Directory.Exists(pathToFolder);
+        }
+    }
+}
---
+++ CSharpHW/26/TPLUnarchiverConsoleApplication/TPLUnarchiverConsoleApplication/Unarchiver.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TPLUnarchiverConsoleApplication
+{
+    class Unarchiver
+    {
+        public void UnarchiveTheFolder(string pathToFolder)
+        {
+            var subDirectories = Directory.GetDirectories(pathToFolder);
+            var files = Directory.GetFiles(pathToFolder);
+            var zipFiles = files.Where(file => Path.GetExtension(file) == ".zip").ToList();
+
+            if (zipFiles.Count != 0)
+            {
+                Parallel.ForEach(zipFiles, UnarchiveTheFile);
+            }
+
+            if (subDirectories.Length == 0)
+                return;
+
+            Parallel.ForEach(subDirectories, UnarchiveTheFolder);
+        }
+
+        private void UnarchiveTheFile(string pathToFile)
+        {
+            var currentDirectoryName = Directory.GetCurrentDirectory();
+            var currentDirectory = new DirectoryInfo(currentDirectoryName);
+            var directoryToExtract = Directory.CreateDirectory(currentDirectory + "\\" + "DirectoryToExtract");
+            ZipFile.ExtractToDirectory(pathToFile, directoryToExtract.FullName);
+        }
+    }
+}
